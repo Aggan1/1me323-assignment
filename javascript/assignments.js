3b079e5886bac976b190fdefcc67907c6953a354
Axel Nilsson <an225ab@student.lnu.se>
2026-02-06 19:25:30 +0100

    main: fixed failed to get page

--- javascript/assignments.js
+++ javascript/assignments.js
@@ -8,13 +8,13 @@ export const assignments = [
   {
     id: "assignment1",
     title: "Uppgift 1",
-    link: "/assignment1/index.html",
+    link: "/1me323-assignment/assignment1/index.html",
     description: "En landningssida som samlar alla inlämningsuppgifter i kursen på en sida."
   },
   {
     id: "assignment2",
     title: "Uppgift 2",
-    link: "/assignment2/index.html",
+    link: "/1me323-assignment/assignment2/index.html",
     description: "Denna uppgiften går ej att öppna utan kommer att läggas till senare."
   }
 ];

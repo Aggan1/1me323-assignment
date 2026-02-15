962497b9e7ba609871eca55afa2aa7cc3f208dba
Axel Nilsson <an225ab@student.lnu.se>
2026-02-15 20:43:06 +0100

    Main: Fixad navigation (sökväg)

--- javascript/assignments.js
+++ javascript/assignments.js
@@ -2,19 +2,19 @@ export const assignments = [
   {
     id: "start",
     title: "Start",
-    link: "/1me323-assignment/index.html",
+    link: "index.html",
     description: ""
   },
   {
     id: "assignment1",
     title: "Uppgift 1",
-    link: "/1me323-assignment/assignment1/index.html",
+    link: "assignment1/index.html",
     description: "En landningssida som samlar alla inlämningsuppgifter i kursen på en sida."
   },
   {
     id: "assignment2",
     title: "Uppgift 2",
-    link: "/1me323-assignment/assignment2/index.html",
-    description: "Denna uppgiften går ej att öppna utan kommer att läggas till senare."
+    link: "assignment2/index.html",
+    description: "Produktsida med kundvagn, lagersaldo och sparad genom localstorage (JSON)."
   }
 ];
--- javascript/navigation.js
+++ javascript/navigation.js
@@ -2,13 +2,19 @@ export function createNavigation(assignments, navElement, currentPage) {
 
   let html = "";
 
-  assignments.forEach(item => {
+  assignments.forEach(function (item) {
+
+    let url = item.link;
+
+    if (currentPage !== "start") {
+      url = "../" + url;
+    }
 
     const aktivKlass = item.id === currentPage ? "aktiv" : "";
 
     html += `
       <li>
-        <a href="${item.link}" class="${aktivKlass}">
+        <a href="${url}" class="${aktivKlass}">
           ${item.title}
         </a>
       </li>
